bd66a58f84e29e45d4de571f517bbfc1a336190d
Lauri Hakkarainen <bionik@gmail.com>
2015-11-30 23:09:47 +0200

    Added heater request

---
+++ heater/index.php
@@ -0,0 +1,40 @@
+<?php
+# Controls heater
+define('HEATER_CLIENT_LOCATION', '/usr/local/bin/gpio');
+
+//Set content type
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+//Response array and shorthand for request
+$response = array();
+$r = $_REQUEST;
+
+//If a parameter is set
+if(isset($r) && isset($r['a'])){
+    $a = $r['a'];
+
+    //getTemp action
+    if($a == 'startHeater'){
+
+      //Start heater
+      shell_exec(HEATER_CLIENT_LOCATION.' on');
+      $response['status'] = 'OK';
+
+    } else if($a == 'stopHeater'){
+
+      //Stop heater
+      shell_exec(HEATER_CLIENT_LOCATION.' off');
+      $response['status'] = 'OK';
+
+    }
+
+}
+
+//If there's no response, do FAIL
+if(count($response) === 0){
+    $response['status'] = 'FAIL';
+    $response['message'] = 'PARAMETER_ERROR';
+}
+
+die(json_encode($response));
