bc08a107f92731aac3167d1260a921e2ae7a7e36
Lauri Hakkarainen <bionik@gmail.com>
2016-04-14 16:10:36 +0300

    PIR

---
+++ pir/index.php
@@ -0,0 +1,36 @@
+<?php
+# Controls heater
+define('STATUS_FILE_LOCATION', '/home/late/dev/pir/status.txt');
+
+//Set content type
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+//Response array and shorthand for request
+$response = array();
+$r = $_REQUEST;
+
+//If a parameter is set
+if(isset($r) && isset($r['a'])){
+    $a = $r['a'];
+
+    if($a == 'getStatus') {
+
+      $heater = array();
+
+      $status = file_get_contents(STATUS_FILE_LOCATION);
+
+      $response['status'] = 'OK';
+      $response['timestamp'] = $status;
+      $response['time'] = date('H:i \o\n l', (int)$status);
+    }
+
+}
+
+//If there's no response, do FAIL
+if(count($response) === 0){
+    $response['status'] = 'FAIL';
+    $response['message'] = 'PARAMETER_ERROR';
+}
+
+die(json_encode($response));
